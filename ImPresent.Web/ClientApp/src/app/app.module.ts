78091fc1ef8fc85ae8e416d8ef8c9a9295416e7b
JM REMEUR <jm@remeur.fr>
2021-02-18 17:40:59 +0100

    client app with tailwindcss create home & create promotion pages

--- ImPresent.Web/ClientApp/src/app/app.module.ts
+++ ImPresent.Web/ClientApp/src/app/app.module.ts
@@ -1,32 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HomeModule } from './pages/home/home.module';
+import { PromotionModule } from './pages/promotion/promotion.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HomeModule,
+    PromotionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
