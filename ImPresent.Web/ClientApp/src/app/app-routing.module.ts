8cc9d340104853f8fc43aa3130403154ca0f5e3d
JM REMEUR <jm@remeur.fr>
2021-02-18 21:57:51 +0100

    create add student view

--- ImPresent.Web/ClientApp/src/app/app-routing.module.ts
+++ ImPresent.Web/ClientApp/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CreatePromotionComponent } from './pages/promotion/create-promotion/create-promotion.component';
 import { ViewPromotionComponent } from './pages/promotion/view-promotion/view-promotion.component';
+import { AddStudentComponent } from './pages/promotion/add-student/add-student.component';
 
 const routes: Routes = [
   { path: 'promotion/create', component: CreatePromotionComponent },
-  { path: 'promotion/:id', component: ViewPromotionComponent },
+  { path: 'promotion/view/:id', component: ViewPromotionComponent },
+  { path: 'promotion/student/add', component: AddStudentComponent },
   { path: '**', component: HomeComponent },
 ];
 
