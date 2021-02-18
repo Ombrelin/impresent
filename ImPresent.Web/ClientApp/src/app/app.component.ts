db584900ebbe76e23cd3fa9ad8c88270bb2f1c03
JM REMEUR <jm@remeur.fr>
2021-02-18 20:11:56 +0100

    moved particles background to app

--- ImPresent.Web/ClientApp/src/app/app.component.ts
+++ ImPresent.Web/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { particlesOptions } from './core/utils/utils';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  particlesOptions = particlesOptions;
 }
--- ImPresent.Web/ClientApp/src/app/app.module.ts
+++ ImPresent.Web/ClientApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgParticlesModule } from 'ng-particles';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { PromotionModule } from './pages/promotion/promotion.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    NgParticlesModule,
     HomeModule,
     PromotionModule
   ],
