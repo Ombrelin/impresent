1d8cea9cd4b996acf3ff7645f0ca53680287425a
Arsène Lapostolet <arsene@lapostolet.fr>
2021-02-19 21:15:56 +0100

    add id in login

--- ImPresent.Web/Model/Dtos/TokenDto.cs
+++ ImPresent.Web/Model/Dtos/TokenDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Impresent.Web.Model.Dtos
+﻿using System;
+
+namespace Impresent.Web.Model.Dtos
 {
     public class TokenDto
     {
         public string Token { get; set; }
+        public Guid Id { get; set; }
     }
 }
