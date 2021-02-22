b8fd442fe1acf77cd98590dd45ec2d4ef2ff69f9
JM REMEUR <jm@remeur.fr>
2021-02-22 01:06:41 +0100

    update promotion to add day update design when loading

--- ImPresent.Web/ClientApp/src/app/pages/promotion/promotion.module.ts
+++ ImPresent.Web/ClientApp/src/app/pages/promotion/promotion.module.ts
@@ -6,17 +6,23 @@ import { MatListModule } from '@angular/material/list';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PromotionComponent } from './promotion.component';
 import { CreatePromotionDialogComponent } from '../home/dialogs/create-promotion-dialog/create-promotion-dialog.component';
 import { AddStudentDialogComponent } from './dialogs/add-student-dialog/add-student-dialog.component';
+import { VolunteerComponent } from './volunteer/volunteer.component';
+import { AddDayDialogComponent } from './dialogs/add-day-dialog/add-day-dialog.component';
 
 @NgModule({
   declarations: [
     PromotionComponent,
     CreatePromotionDialogComponent,
-    AddStudentDialogComponent
+    AddStudentDialogComponent,
+    VolunteerComponent,
+    AddDayDialogComponent,
   ],
   imports: [
     CommonModule,
@@ -26,7 +32,9 @@ import { AddStudentDialogComponent } from './dialogs/add-student-dialog/add-stud
     ReactiveFormsModule,
     MatListModule,
     MatIconModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
 })
 export class PromotionModule { }
