097a22a1596205368d5e859a29ea6a897713ae34
Arsène Lapostolet <arsene@lapostolet.fr>
2021-02-22 09:56:02 +0100

    add migration

---
+++ ImPresent.Web/Migrations/20210222085518_presenceDays.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace impresent.Migrations
+{
+    public partial class presenceDays : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PresenceDay",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    PromotionId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PresenceDay", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PresenceDay_Promotions_PromotionId",
+                        column: x => x.PromotionId,
+                        principalTable: "Promotions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PresenceDay_PromotionId",
+                table: "PresenceDay",
+                column: "PromotionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PresenceDay");
+        }
+    }
+}
