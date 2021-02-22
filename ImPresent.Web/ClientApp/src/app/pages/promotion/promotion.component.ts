3fc0431ab2b0187a5c5602784f739ea1a64fa261
JM REMEUR <jm@remeur.fr>
2021-02-22 20:14:27 +0100

    refactor promotion to display days instead of students

--- ImPresent.Web/ClientApp/src/app/pages/promotion/promotion.component.ts
+++ ImPresent.Web/ClientApp/src/app/pages/promotion/promotion.component.ts
@@ -5,11 +5,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/core/http/api.service';
 import { DialogService } from 'src/app/core/services/dialog/dialog.service';
 import { SnackbarService } from 'src/app/core/services/snackbar/snackbar.service';
-import { StorageService } from 'src/app/core/services/storage/storage.service';
 import { LoadingDialogComponent } from 'src/app/shared/components/dialogs/loading-dialog/loading-dialog.component';
 import { AddStudentDialogComponent } from './dialogs/add-student-dialog/add-student-dialog.component';
 import { AddDayDialogComponent } from './dialogs/add-day-dialog/add-day-dialog.component';
-import { PromotionDto } from 'src/app/shared/models/model';
+import { PromotionDto, DayDto } from 'src/app/shared/models/model';
 
 
 @Component({
@@ -19,33 +18,29 @@ import { PromotionDto } from 'src/app/shared/models/model';
 })
 export class PromotionComponent implements OnInit {
 
-  students: Array<string> = [];
   loaded = false;
+  error: string | null = null;
   promotion: PromotionDto = {
     id: '',
     className: '',
     students: [],
     presenceDays: []
   };
-  private token = '';
 
   constructor(
     private readonly route: ActivatedRoute,
     private readonly router: Router,
     private readonly dialog: MatDialog,
     private readonly dialogService: DialogService,
-    private readonly storageService: StorageService,
     private readonly snackbarService: SnackbarService,
     private readonly api: ApiService
   ) {
-    const token = this.storageService.getToken();
-    if (token != null) {
-      this.token = token;
-    }
-
     this.route.params.subscribe((params) => {
-      if (params.id != null) {
-        this.fetch(params.id, true);
+      if (params.promotionId != null) {
+        this.fetch(params.promotionId, true);
+      }
+      else {
+        this.router.navigate(['']);
       }
     });
   }
@@ -58,41 +53,46 @@ export class PromotionComponent implements OnInit {
     return new Date(date);
   }
 
-  private async fetch(id: string, first = false): Promise<void> {
+  private async fetch(promotionId: string, first = false): Promise<void> {
     let loadingDialog: MatDialogRef<LoadingDialogComponent> | null = null;
     if (first) {
       loadingDialog = this.dialogService.showLoading();
     }
-    let error: string | null = null;
+    let snackBarError: string | null = null;
     try {
-      const res = await this.api.getPromotion(id);
+      const res = await this.api.getPromotion(promotionId);
 
       if (res.status === 200) {
-        this.loaded = true;
         this.promotion = res.data;
       }
       else if (res.status === 401) {
-        error = 'Expired token';
+        snackBarError = 'Expired token';
         this.router.navigate(['']);
       }
       else {
-        error = `${res.status} : ${res.data}`;
+        this.error = `${res.status} : ${res.data}`;
       }
     }
     catch (e) {
-      error = 'Request timeout';
+      this.error = 'Request timeout';
       this.router.navigate(['']);
     }
+
+    this.loaded = true;
 
     if (first && loadingDialog != null) {
       loadingDialog.close();
     }
 
-    if (error != null) {
-      this.snackbarService.show(error, {
+    if (snackBarError != null) {
+      this.snackbarService.show(snackBarError, {
         duration: 3000
       });
     }
+  }
+
+  openDay(day: DayDto): void {
+    this.router.navigate(['promotion', this.promotion.id, 'day', day.id]);
   }
 
   addDay(): void {
