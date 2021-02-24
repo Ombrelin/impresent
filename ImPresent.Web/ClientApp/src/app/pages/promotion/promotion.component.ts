85297491a7c3c7f08f2771ae8becceba7c22d6cd
JM REMEUR <jm@remeur.fr>
2021-02-24 01:36:27 +0100

    remove imports & useless method

--- ImPresent.Web/ClientApp/src/app/pages/promotion/promotion.component.ts
+++ ImPresent.Web/ClientApp/src/app/pages/promotion/promotion.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { ApiService } from 'src/app/core/http/api.service';
-import { DialogService } from 'src/app/core/services/dialog/dialog.service';
 import { SnackbarService } from 'src/app/core/services/snackbar/snackbar.service';
-import { LoadingDialogComponent } from 'src/app/shared/components/dialogs/loading-dialog/loading-dialog.component';
 import { AddStudentDialogComponent } from './dialogs/add-student-dialog/add-student-dialog.component';
 import { AddDayDialogComponent } from './dialogs/add-day-dialog/add-day-dialog.component';
 import { PromotionDto, DayDto } from 'src/app/shared/models/model';
@@ -27,9 +24,7 @@ export class PromotionComponent implements OnInit {
     private readonly route: ActivatedRoute,
     private readonly router: Router,
     private readonly dialog: MatDialog,
-    private readonly dialogService: DialogService,
     private readonly snackbarService: SnackbarService,
-    private readonly api: ApiService,
     private readonly stateService: StateService
   ) {
     this.route.params.subscribe(async (params) => {
@@ -43,9 +38,7 @@ export class PromotionComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void { }
 
   toDate(date: string): Date {
     return new Date(date);
@@ -62,45 +55,11 @@ export class PromotionComponent implements OnInit {
       });
       this.router.navigate(['']);
     }
-    else {
+    else if (state.success) {
       this.promotion = state.data;
     }
-  }
-
-  private async fetch(promotionId: string, first = false): Promise<void> {
-    let loadingDialog: MatDialogRef<LoadingDialogComponent> | null = null;
-    if (first) {
-      loadingDialog = this.dialogService.showLoading();
-    }
-    let snackBarError: string | null = null;
-    try {
-      const res = await this.api.getPromotion(promotionId);
-
-      if (res.status === 200) {
-        this.promotion = res.data;
-      }
-      else if (res.status === 401) {
-        snackBarError = 'Expired token';
-      }
-      else {
-        this.error = `${res.status} : ${res.data}`;
-      }
-    }
-    catch (e) {
-      snackBarError = 'Request timeout';
-    }
-
-    this.loaded = true;
-
-    if (first && loadingDialog != null) {
-      loadingDialog.close();
-    }
-
-    if (snackBarError != null) {
-      this.snackbarService.show(snackBarError, {
-        duration: 3000
-      });
-      this.router.navigate(['']);
+    else {
+      this.error = 'Invalid promotion';
     }
   }
 
