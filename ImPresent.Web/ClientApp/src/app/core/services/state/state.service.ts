d13a07990579faf9ae0e05c24ae1cdced6090fae
JM REMEUR <jm@remeur.fr>
2021-02-25 00:16:16 +0100

    remove cache service

--- ImPresent.Web/ClientApp/src/app/core/services/state/state.service.ts
+++ ImPresent.Web/ClientApp/src/app/core/services/state/state.service.ts
@@ -3,10 +3,8 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { ApiResponse } from 'ts-retrofit2';
 
 import { PromotionDto } from 'src/app/shared/models/model';
-import { ApiService } from 'src/app/core/http/api.service';
 import { DialogService } from 'src/app/core/services/dialog/dialog.service';
 import { LoadingDialogComponent } from 'src/app/shared/components/dialogs/loading-dialog/loading-dialog.component';
-import { CacheService } from 'src/app/core/services/cache/cache.service';
 
 export interface State<T> {
   data?: T;
@@ -16,7 +14,7 @@ export interface State<T> {
   success: boolean;
 }
 
-const invalidPromotionId: State<PromotionDto> = {
+export const invalidPromotionId: State<PromotionDto> = {
   snackbarError: 'Invalid promotion id',
   success: false
 };
@@ -27,39 +25,10 @@ const invalidPromotionId: State<PromotionDto> = {
 export class StateService {
 
   constructor(
-    private readonly api: ApiService,
     private readonly dialogService: DialogService,
-    private readonly cacheService: CacheService
   ) { }
 
-  async getPromotion(promotionId: string | null | undefined, loading = false): Promise<State<PromotionDto>> {
-    if (promotionId == null) {
-      return invalidPromotionId;
-    }
-    const promotion = this.cacheService.getPromotion(promotionId);
-    let state: State<PromotionDto> = {
-      data: promotion,
-      success: promotion != null
-    };
-    if (promotion == null) {
-      state = await this.updatePromotion(promotionId, loading);
-    }
-
-    return state;
-  }
-
-  async updatePromotion(promotionId: string | null | undefined, loading = false): Promise<State<PromotionDto>> {
-    if (promotionId == null) {
-      return invalidPromotionId;
-    }
-    const state = await this.fetch<PromotionDto>(this.api.getPromotion(promotionId), loading);
-    if (state.data != null) {
-      this.cacheService.setPromotion(state.data);
-    }
-    return state;
-  }
-
-  private async fetch<T>(promise: ApiResponse<T>, loading = false): Promise<State<T>> {
+  async fetch<T>(promise: ApiResponse<T>, loading = false): Promise<State<T>> {
     let loadingDialog: MatDialogRef<LoadingDialogComponent> | null = null;
     if (loading) {
       loadingDialog = this.dialogService.showLoading();
