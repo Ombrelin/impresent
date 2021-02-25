0f1baefd1950301d3d75edd1454f1a339c922156
JM REMEUR <jm@remeur.fr>
2021-02-26 00:07:16 +0100

    remove page import

--- ImPresent.Web/ClientApp/src/app/shared/shared.module.ts
+++ ImPresent.Web/ClientApp/src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { InputDirective } from './directives/input-directive/input.directive';
 import { ButtonDirective } from './directives/button-directive/button.directive';
 import { LoadingDialogComponent } from './components/dialogs/loading-dialog/loading-dialog.component';
-import { Page, PageComponent } from './components/page/page.component';
+import { PageComponent } from './components/page/page.component';
 
 @NgModule({
   imports: [
