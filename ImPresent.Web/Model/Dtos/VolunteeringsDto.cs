e847c693f4e2af8df50a0be0e717c573f8f1ef8f
Arsène Lapostolet <arsene@lapostolet.fr>
2021-02-25 09:59:12 +0100

    finished

---
+++ ImPresent.Web/Model/Dtos/VolunteeringsDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Impresent.Web.Model.Dtos
+{
+    public class VolunteeringsDto
+    {
+        public PresenceDayDto PresenceDay { get; set; }
+        public List<StudentDto> Students { get; set; }
+    }
+}
